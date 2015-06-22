8592313d3175a5733a78844f99976a8f85134fae
Vadim Brodsky <vadimb@crankworks.ca>
2015-06-22 18:23:29 -0400

    Hook Firebase to app

--- 08-todo/main.js
+++ 08-todo/main.js
@@ -20244,9 +20244,17 @@ var ReactFireMixin = {
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var rootUrl = 'https://amber-heat-8131.firebaseio.com/';
 
 var App = React.createClass({displayName: "App",
+  mixins: [ ReactFire ],
+  componentWillMount: function() {
+    // bindAsObject comes from ReactFire
+    // this.state.items => {}
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
+    console.log(this.state);
     return React.createElement("h1", null, 
       "Hello!"
     )
--- 08-todo/src/app.jsx
+++ 08-todo/src/app.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var rootUrl = 'https://amber-heat-8131.firebaseio.com/';
 
 var App = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function() {
+    // bindAsObject comes from ReactFire
+    // this.state.items => {}
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
+    console.log(this.state);
     return <h1>
       Hello!
     </h1>
